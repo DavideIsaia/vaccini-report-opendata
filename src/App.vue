65d657d488099e0ab0f621a8c8ba1fdca7678b9f
DavideIsaia <davide.enjoy@hotmail.it>
2022-09-26 16:07:26 +0200

    aggiunte immagini e chiamate api (controllare se sono decaduti i percorsi delle 2 non funzionanti)

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,39 @@
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
+import api from "./api";
 
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      vacciniDataSet: [],
+      somministrazioniDataSet: [],
+      anagraficaDataSet: [],
+      consegneDataSet: [],
+    };
+  },
+  methods: {
+    // aggiungiamo await in tutte le chiamate per evitare che rimangano delle promise
+    async loadData() {
+      this.vacciniDataSet = await api.readFile(
+        `${api.API_URL}${api.VACCINI_SUMMARY_PATH}`
+      );
+      this.somministrazioniDataSet = await api.readFile(
+        `${api.API_URL}${api.SOMMINISTRAZIONI_SUMMARY_PATH}`
+      );
+      this.anagraficaDataSet = await api.readFile(
+        `${api.API_URL}${api.ANAGRAFICA_SUMMARY_PATH}`
+      );
+      this.consegneDataSet = await api.readFile(
+        `${api.API_URL}${api.CONSEGNE_VACCINI_PATH}`
+      );
+    },
+  },
+  mounted() {
+    this.loadData();
+  },
 };
 </script>
 
--- src/api.js
+++ src/api.js
@@ -1,8 +1,21 @@
-const API_URL = "https://api.github.com/repos/italia/covid19-opendata-vaccini/contents/";
+const API_URL =
+  "https://api.github.com/repos/italia/covid19-opendata-vaccini/contents/";
 const VACCINI_SUMMARY_PATH = "dati/vaccini-summary-latest.json";
-const SOMMINISTRAZIONI_SUMMARY_PATH = "dati/somministrazioni-vaccini-summary-latest.json";
+const SOMMINISTRAZIONI_SUMMARY_PATH =
+  "dati/somministrazioni-vaccini-summary-latest.json";
 const ANAGRAFICA_SUMMARY_PATH = "dati/anagrafica-vaccini-summary-latest.json";
 const CONSEGNE_VACCINI_PATH = "dati/consegne-vaccini-latest.json";
+
+const readFile = async (url) => {
+  let result = null;
+  const response = await fetch(url);
+  const json = await response.json();
+  if (json.content) {
+    result = JSON.parse(atob(json.content)).data;
+    // i file opendata sui vaccini di github sono in base 64, quindi col metodo atob lo converto da base64 a stringa e poi lo converto in json con json.parse e di questo json andiamo a prendere solo i .data
+  }
+  return result;
+};
 
 export default {
   API_URL,
@@ -10,4 +23,5 @@ export default {
   SOMMINISTRAZIONI_SUMMARY_PATH,
   ANAGRAFICA_SUMMARY_PATH,
   CONSEGNE_VACCINI_PATH,
+  readFile,
 };
